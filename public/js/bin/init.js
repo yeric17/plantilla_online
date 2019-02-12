d99dfa3d7f6773ed574f7b98e797026a831ff981
yeric17 <yeric17@gmail.com>
2019-02-12 17:25:55 -0500

    se crea la plantilla para preguntas abiertas

---
+++ public/js/bin/init.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems,{direction: 'left'});
+    var textAreas = document.getElementsByTagName("textarea")
+    for(var x = 0; x < textAreas.length; x++){
+        textAreas[x].classList.add("materialize-textarea")
+    }
+  });
