917683afb99e78703fee84fd161bd93eedbcc45e
yeric17 <yeric17@gmail.com>
2019-02-13 11:29:16 -0500

    time picker

--- public/js/bin/init.js
+++ public/js/bin/init.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var FloatButton = M.FloatingActionButton.init(elems,{direction: 'left'});
-    var elems = document.querySelectorAll('.datepicker');
+    
+    elems = document.querySelectorAll('.datepicker');
     var DatePicker = M.Datepicker.init(elems, {format: "dd/mm/yyyy"});
 
+    var elems = document.querySelectorAll('.timepicker');
+    var TimePicker = M.Timepicker.init(elems, {defaultTime:"now"});
 
     var textAreas = document.getElementsByTagName("textarea")
     for(var x = 0; x < textAreas.length; x++){
