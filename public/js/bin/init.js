6719aabab42ba0b908e94122013a3fddac9dd4a1
yeric17 <yeric17@gmail.com>
2019-02-13 16:57:23 -0500

    se agrega template para multiples

--- public/js/bin/init.js
+++ public/js/bin/init.js
@@ -6,4 +6,27 @@ document.addEventListener('DOMContentLoaded', function() {
     for(var x = 0; x < textAreas.length; x++){
         textAreas[x].classList.add("materialize-textarea")
     }
+
+    var mrQuestionTable = document.getElementsByClassName("mrQuestionTable")[0]
+    var spans
+    if(mrQuestionTable !== undefined){
+        spans= mrQuestionTable.getElementsByTagName("span")
+    }
+    if(spans){
+        for(var x = 0; x < spans.length; x++){
+    
+            var input = spans[x].getElementsByClassName("mrMultiple")[0] || undefined
+            
+            if(input !== undefined){
+                label = spans[x].getElementsByTagName("label")[0]
+                var innSpan = label.getElementsByTagName("span")[0]
+    
+                label.removeChild(innSpan);
+                spans[x].removeChild(input);
+                label.appendChild(input);
+                label.appendChild(innSpan);
+            }
+        }
+    }
+
   });
