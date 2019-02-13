3ec9ee0d4c822da93651cdbdad11c0e768b8212b
yeric17 <yeric17@gmail.com>
2019-02-13 11:22:12 -0500

    se agrega template para fechas

--- public/js/bin/init.js
+++ public/js/bin/init.js
@@ -1,6 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems,{direction: 'left'});
+    var FloatButton = M.FloatingActionButton.init(elems,{direction: 'left'});
+    var elems = document.querySelectorAll('.datepicker');
+    var DatePicker = M.Datepicker.init(elems, {format: "dd/mm/yyyy"});
+
+
     var textAreas = document.getElementsByTagName("textarea")
     for(var x = 0; x < textAreas.length; x++){
         textAreas[x].classList.add("materialize-textarea")
