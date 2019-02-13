7f5940d15b20c8ecb74eb9107aa649d619141d8f
yeric17 <yeric17@gmail.com>
2019-02-13 13:02:24 -0500

    se ajusta el español para las preguntas fecha

--- public/js/bin/init.js
+++ public/js/bin/init.js
@@ -1,14 +1,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.fixed-action-btn');
     var FloatButton = M.FloatingActionButton.init(elems,{direction: 'left'});
-    
-    elems = document.querySelectorAll('.datepicker');
-    var DatePicker = M.Datepicker.init(elems, {format: "dd/mm/yyyy"});
 
-    var elems = document.querySelectorAll('.timepicker');
-    var TimePicker = M.Timepicker.init(elems, {defaultTime:"now"});
-
-    var textAreas = document.getElementsByTagName("textarea")
+    var textAreas = document.getElementsByTagName("textarea");
     for(var x = 0; x < textAreas.length; x++){
         textAreas[x].classList.add("materialize-textarea")
     }
